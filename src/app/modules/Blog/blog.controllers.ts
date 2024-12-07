b49933ce7331f5bb832860b0df2d954689a2c872
gjsoy24 <gour.joy24@gmail.com>
2024-12-07 22:26:33 +0600

    Refactor blog module: improve response structure, enhance pagination metadata, and apply consistent formatting

--- src/app/modules/Blog/blog.controllers.ts
+++ src/app/modules/Blog/blog.controllers.ts
@@ -1,105 +1,83 @@
-import { Request, RequestHandler, Response } from "express";
-import httpStatus from "http-status";
-import catchAsync from "../../utils/catchAsync";
-import sendResponse from "../../utils/sendResponse";
-import { BlogServices } from "./blog.services";
+import { Request, RequestHandler, Response } from 'express';
+import httpStatus from 'http-status';
+import catchAsync from '../../utils/catchAsync';
+import sendResponse from '../../utils/sendResponse';
+import { BlogServices } from './blog.services';
 
-const createBlog: RequestHandler = catchAsync(
-  async (req: Request, res: Response) => {
-    const blogData = req.body;
+const createBlog: RequestHandler = catchAsync(async (req: Request, res: Response) => {
+	const blogData = req.body;
 
-    const result = await BlogServices.createBlog(blogData);
+	const result = await BlogServices.createBlog(blogData);
 
-    sendResponse(res, {
-      statusCode: httpStatus.CREATED,
-      success: true,
-      message: "Blog created successfully!",
-      data: result,
-    });
-  },
-);
+	sendResponse(res, {
+		statusCode: httpStatus.CREATED,
+		success: true,
+		message: 'Blog created successfully!',
+		data: result
+	});
+});
 
-const updateBlog: RequestHandler = catchAsync(
-  async (req: Request, res: Response) => {
-    const blogId = req.params.id;
-    const updateData = req.body; 
-    const updatedBlog = await BlogServices.updateBlog(blogId, updateData);
+const updateBlog: RequestHandler = catchAsync(async (req: Request, res: Response) => {
+	const blogId = req.params.id;
+	const updateData = req.body;
+	const updatedBlog = await BlogServices.updateBlog(blogId, updateData);
 
-    sendResponse(res, {
-      statusCode: httpStatus.OK,
-      success: true,
-      message: "Blog updated successfully!",
-      data: updatedBlog,
-    });
-  },
-);
+	sendResponse(res, {
+		statusCode: httpStatus.OK,
+		success: true,
+		message: 'Blog updated successfully!',
+		data: updatedBlog
+	});
+});
 
-const getBlogs: RequestHandler = catchAsync(
-  async (req: Request, res: Response) => {
-    const searchQuery = req.query;
+const getBlogs: RequestHandler = catchAsync(async (req: Request, res: Response) => {
+	const searchQuery = req.query;
 
-    const page = parseInt(req.query.page as string) || 1;
-    const limit = parseInt(req.query.limit as string) || 10;
-    const skip = (page - 1) * limit;
+	const page = parseInt(req.query.page as string) || 1;
+	const limit = parseInt(req.query.limit as string) || 10;
+	const skip = (page - 1) * limit;
 
-    const { blogs, meta } = await BlogServices.getBlogs(
-      searchQuery,
-      skip,
-      limit,
-    );
+	const { blogs, meta } = await BlogServices.getBlogs(searchQuery, skip, limit);
 
-    sendResponse(res, {
-      statusCode: httpStatus.OK,
-      success: true,
-      message: "Blogs retrieved successfully!",
-      data: {
-        blogs,
-        pagination: {
-          totalBlogs: meta.total, 
-          totalPages: meta.totalPage,  
-          currentPage: page,  
-          limit, 
-        },
-      },
-    });
-  }
-);
+	sendResponse(res, {
+		statusCode: httpStatus.OK,
+		success: true,
+		message: 'Blogs retrieved successfully!',
+		meta,
+		data: blogs
+	});
+});
 
+const getBlog: RequestHandler = catchAsync(async (req: Request, res: Response) => {
+	const blogId = req.params.id;
 
-const getBlog: RequestHandler = catchAsync(
-  async (req: Request, res: Response) => {
-    const blogId = req.params.id;
+	const result = await BlogServices.getBlogById(blogId);
 
-    const result = await BlogServices.getBlogById(blogId);
+	sendResponse(res, {
+		statusCode: httpStatus.OK,
+		success: true,
+		message: 'Blog retrieved successfully!',
+		data: result
+	});
+});
 
-    sendResponse(res, {
-      statusCode: httpStatus.OK,
-      success: true,
-      message: "Blog retrieved successfully!",
-      data: result,
-    });
-  },
-);
+const deleteBlog: RequestHandler = catchAsync(async (req: Request, res: Response) => {
+	const blogId = req.params.id;
 
-const deleteBlog: RequestHandler = catchAsync(
-  async (req: Request, res: Response) => {
-    const blogId = req.params.id;
+	const result = await BlogServices.deleteBlog(blogId);
 
-    const result = await BlogServices.deleteBlog(blogId);
-
-    sendResponse(res, {
-      statusCode: httpStatus.OK,
-      success: true,
-      message: "Blog deleted successfully!",
-      data: result,
-    });
-  },
-);
+	sendResponse(res, {
+		statusCode: httpStatus.OK,
+		success: true,
+		message: 'Blog deleted successfully!',
+		data: result
+	});
+});
 
 export const BlogControllers = {
-  createBlog,
-  updateBlog,
-  getBlogs,
-  getBlog,
-  deleteBlog,
+	createBlog,
+	updateBlog,
+	getBlogs,
+	getBlog,
+	deleteBlog
 };
