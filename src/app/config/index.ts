d137f358dc487c1b4474587d946dc4f612e6f5d9
gjsoy24 <gour.joy24@gmail.com>
2024-12-21 00:30:50 +0600

    Set default NODE_ENV to 'development' and add error handling for missing database URL

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -6,7 +6,7 @@ dotenv.config({ path: path.join(process.cwd(), '.env') });
 export default {
 	port: process.env.PORT || 5000,
 	database_url: process.env.DATABASE_URL,
-	NODE_ENV: process.env.NODE_ENV,
+	NODE_ENV: process.env.NODE_ENV || 'development',
 	bcrypt_salt_round: process.env.BCRYPT_SALT_ROUND,
 	jwt_secret: process.env.JWT_SECRET as string,
 	jwt_expiration: process.env.JWT_EXPIRATION as string,
